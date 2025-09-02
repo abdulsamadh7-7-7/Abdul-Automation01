d2c1e8633338642fc992c02457c53378766141ab
Shaik <abdul.samadh.shaik@accenture.com>
2025-09-02 20:36:23 +0530

    Update .gitignore and remove ignored files

--- src/test/java/com/abdul/tests/test1.java
+++ src/test/java/com/abdul/tests/test1.java
@@ -2,13 +2,17 @@ package com.abdul.tests;
 
 
 
+
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 
 public class test1 {
     
@@ -17,18 +21,19 @@ public class test1 {
     @BeforeMethod
     public void setUp() {
         // Automatically sets up the ChromeDriver binary
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    	WebDriverManager.chromedriver().setup();
+        String tempdir = System.getProperty("java.io.tmpdir");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("user-data-dir=" + tempdir + "chrome-data");
+
+        driver = new ChromeDriver(options);
     }
-    
-    @Test
+    @Test 
     public void openGooglePage() {
         driver.get("https://www.google.com");
         String title = driver.getTitle();
         Assert.assertTrue(title.contains("Google"), "Title should contain 'Google'");
     }
-    
     @Test
     public void openYouTubePage() {
         driver.get("https://www.youtube.com");
